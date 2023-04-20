57ea2fda3c8698aa219adf72edc1a9ab12b3fec9
plamen-peshev <plamen.peshev@live.com>
2023-04-20 12:00:15 +0300

    new

--- OrgChartUploadImageNETCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OrgChartUploadImageNETCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
